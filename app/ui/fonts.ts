7d68683f340ffbc3c5226121516982fa60b4cc6b
raoyi <2196272354@qq.com>
2023-10-28 11:58:55 +0800

    第一次提交nextjs项目

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lunasima } from 'next/font/google'
+
+// 拉丁字体
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lunasima({
+    weight: [ '400', '700' ],
+    subsets: [ 'latin' ]
+})
